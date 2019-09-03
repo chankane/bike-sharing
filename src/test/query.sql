2db5621f4f9d86b5f94922f88ca8d03100e1f333
chankane <brawnychocolate@gmail.com>
2019-09-03 23:18:44 +0900

    Processing...

--- src/test/query.sql
+++ src/test/query.sql
@@ -1,3 +1,72 @@
-select * from usta;
-select * from utrip;
-select * from usta, utrip where (usta.id=utrip.start_sta_id or usta.id=utrip.end_sta_id) group by city;
+/*count(distinct(utrip.id, city))*/
+select
+  utrip.id,
+  city,
+  count(utip.id, city)
+from
+  utrip,
+  usta
+where
+  usta.id = utrip.start_sta_id
+or  usta.id = utrip.end_sta_id
+group by
+  utrip.id,
+  city
+;
+
+select
+  distinct utrip.id, city
+from
+  utrip,
+  usta
+where
+  usta.id = utrip.start_sta_id
+or  usta.id = utrip.end_sta_id
+;
+
+select
+  city,
+  count(city)
+from
+  (
+    select distinct
+      utrip.id,
+      city
+    from
+      utrip,
+      usta
+    where
+      usta.id = utrip.start_sta_id
+    or  usta.id = utrip.end_sta_id
+  ) as tmp0
+group by
+  city
+;
+
+select
+  city,
+  cast(cnt as float) / count(tmp0.city) as ratio
+from
+  (
+    select
+      city,
+      count(city) as cnt,
+    from
+      (
+        select distinct
+          utrip.id,
+          city
+        from
+          utrip,
+          usta
+        where
+          usta.id = utrip.start_sta_id
+        or  usta.id = utrip.end_sta_id
+      ) as tmp0
+    group by
+      city
+  ) as tmp1
+order by
+  ratio desc,
+  city
+;
